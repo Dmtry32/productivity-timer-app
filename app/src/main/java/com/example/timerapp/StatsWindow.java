4b80d5028339e4bd7e3b748ef723d63f4c25d16e
dmtry32 <anas.moussaoui@ump.ac.ma>
2026-03-07 00:41:22 +0100

    [main]: Fix error cannot find symbol Basic stroke due to unimported library

--- app/src/main/java/com/example/timerapp/StatsWindow.java
+++ app/src/main/java/com/example/timerapp/StatsWindow.java
@@ -6,6 +6,7 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
 import org.jfree.chart.axis.NumberAxis;
+import java.awt.BasicStroke;
 
 import javax.swing.*;
 import java.time.YearMonth;
